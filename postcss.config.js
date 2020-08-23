2c1229857064140a1ce6e9ff68df9ff4328aebae
Prateek Karki <pratiek.karki@gmail.com>
2020-08-23 12:09:56 +0545

    setting up tailwind

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const tailwindConfig = require('./tailwind');
+module.exports = {
+	plugins: {
+		tailwindcss: tailwindConfig,
+		autoprefixer: {},
+		...(process.env.NODE_ENV === 'production'
+			? {
+					'@fullhuman/postcss-purgecss': {
+						content: ['./components/**/*.js', './pages/**/*.js'],
+						defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+					},
+			  }
+			: {}),
+		cssnano: {},
+	},
+};

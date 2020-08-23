984c768c4bde15356a1099e2e996c160621a3ea9
Prateek Karki <pratiek.karki@gmail.com>
2020-08-23 22:58:35 +0545

    fix routing , loader

--- components/query.js
+++ components/query.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import Loader from './Loader';
 
 const Query = ({ children, query, id }) => {
 	const { data, loading, error } = useQuery(query, {
 		variables: { id: id },
 	});
 
-	if (loading) return <p>Loading...</p>;
+	if (loading) return <Loader />;
 	if (error) {
 		console.log(error);
 		return null;

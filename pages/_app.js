14d23c5a391974826af5df89fa903a8ae2f93da0
Prateek Karki <pratiek.karki@gmail.com>
2020-08-23 12:09:56 +0545

    setting up tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,21 @@
-import React from "react";
-import Head from "next/head";
-import { ApolloProvider } from "@apollo/react-hooks";
-import withData from "../utils/apollo";
-import Nav from "../components/Nav";
+import React from 'react';
+import Head from 'next/head';
+import { ApolloProvider } from '@apollo/react-hooks';
+import withData from '../utils/apollo';
+import Nav from '../components/Nav';
+import '../assets/css/tailwind.css';
 
 const App = ({ Component, pageProps, apollo }) => {
-  return (
-    <ApolloProvider client={apollo}>
-      <Head>
-        <title>Strapi blog</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Nav />
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={apollo}>
+			<Head>
+				<title>Strapi blog</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<Nav />
+			<Component {...pageProps} />
+		</ApolloProvider>
+	);
 };
 
 // Wraps all components in the tree with the data provider

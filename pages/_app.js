0c2eb7cb55c7d08bea66c1abe7bf052279d3ce3e
Prateek Karki <pratiek.karki@gmail.com>
2020-08-25 23:57:26 +0545

    add meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,16 @@ const App = ({ Component, pageProps, apollo }) => {
 	return (
 		<ApolloProvider client={apollo}>
 			<Head>
-				<title>Strapi blog</title>
+				<title>Prateek Karki - Full-stack web developer from Kathmandu, Nepal</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta charset="utf-8"></meta>
+				<meta
+					name="description"
+					content="Prateek Karki is a full-stack web developer dedicated to providing highly performant solutions on the internet. 
+					Developing in ever emerging technologies like react JS and Node JS, 
+					he will suggest and provide the best solutions for any problems his clients might have."
+				></meta>
+				<meta name="keywords" content="Web Development, Web Developer, Javascript Developer, Freelancer"></meta>
 			</Head>
 			<Header />
 			<Component {...pageProps} />
--- pages/category/[cid].js
+++ pages/category/[cid].js
@@ -3,6 +3,9 @@ import Articles from '../../components/Articles';
 import Query from '../../components/query';
 import CATEGORY_ARTICLES_QUERY from '../../apollo/queries/category/articles';
 
+import Head from 'next/head';
+import _ from 'lodash';
+
 const Category = () => {
 	const router = useRouter();
 
@@ -10,12 +13,18 @@ const Category = () => {
 		<Query query={CATEGORY_ARTICLES_QUERY} id={router.query.cid}>
 			{({ data: { category } }) => {
 				return (
-					<div className="container mx-auto">
-						<p className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-							#{category.name}
-						</p>
-						<Articles articles={category.articles} />
-					</div>
+					<>
+						<Head>
+							<title>{_.capitalize(category.name)} : Prateek Karki's blog</title>
+						</Head>
+
+						<div className="container mx-auto">
+							<p className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+								#{category.name}
+							</p>
+							<Articles articles={category.articles} />
+						</div>
+					</>
 				);
 			}}
 		</Query>

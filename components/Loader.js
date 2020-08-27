c514dca24cdc2409023c5065c2aa56a5cb54ebdb
Prateek Karki <pratiek.karki@gmail.com>
2020-08-28 00:18:29 +0545

    add loader as tw component

--- components/Loader.js
+++ components/Loader.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import 'twin.macro';
+/** @jsx jsx **/
+import { jsx, css, keyframes } from '@emotion/core';
+import tw from 'twin.macro';
 
 function Loader() {
+	const spinner = keyframes`
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	`;
+
 	return (
 		<div tw="flex h-full items-center justify-center w-full">
-			<div tw="ease-linear rounded-full border-8 border-t-8 border-gray-200 h-10 w-10"></div>
+			<div
+				css={css`
+					${tw`border-gray-200 ease-linear rounded-full border-8 border-t-8  h-10 w-10 `}
+					border-top-color: #3498db;
+					animation: ${spinner} 0.5s linear infinite;
+				`}
+			></div>
 		</div>
 	);
 }

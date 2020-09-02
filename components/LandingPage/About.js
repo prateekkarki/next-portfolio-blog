34b06227b84a7c3b073478fafdb20e465b5d6c78
Prateek Karki <pratiek.karki@gmail.com>
2020-09-02 22:22:28 +0545

    added about section

--- components/LandingPage/About.js
+++ components/LandingPage/About.js
@@ -1,12 +1,29 @@
-import React from 'react';
+/** @jsx jsx **/
+import { jsx } from '@emotion/core';
 
 import TitleBlock from './TitleBlock';
-import tw, { css } from 'twin.macro';
+import tw from 'twin.macro';
 
 function About() {
 	return (
 		<div>
 			<TitleBlock title="About Me" subtitle="Who am I?" />
+			<div css={tw`block md:flex`}>
+				<div css={tw`hidden md:block`}>
+					<img src="/images/coffee-laptop.jpg" alt="laptop coffee" />
+				</div>
+				<div css={tw`text-center bg-main-light text-light py-4`}>
+					<h4 css={tw`text-2xl`}>
+						<strong>Hello stranger,</strong>
+					</h4>
+					<p css={tw`text-base font-normal mt-4`}>
+						I'm Prateek, a web developer from Kathmandu, Nepal.
+						<br />I enjoy creating things that live on the internet, whether that be websites, applications,
+						or anything in between. My goal is to always build products that provide pixel-perfect,
+						performant experiences.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 }

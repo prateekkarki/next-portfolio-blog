3f77006a9abf8c4eb1d49ba5f23e32ea8802021e
Prateek Karki <pratiek.karki@gmail.com>
2020-09-06 01:04:45 +0545

    update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
 		'react/jsx-filename-extension': 0,
 		'react/jsx-indent': 0,
 		'react/jsx-indent-props': 0,
+		'react/jsx-props-no-spreading': 0,
+		'react/jsx-one-expression-per-line': 0,
 		'jsx-a11y/accessible-emoji': 0,
 	},
 };

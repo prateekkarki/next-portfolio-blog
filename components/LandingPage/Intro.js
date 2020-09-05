be3c642fefa53308980fec359a407d0e62e009aa
Prateek Karki <pratiek.karki@gmail.com>
2020-09-06 00:29:23 +0545

    adding and configuring exlint rules

--- components/LandingPage/Intro.js
+++ components/LandingPage/Intro.js
@@ -22,7 +22,8 @@ function Intro() {
 			<div css={tw`container flex h-full items-center justify-center mx-auto`}>
 				<div css={tw`w-full md:w-1/2 text-center md:text-left`}>
 					<p css={tw`text-primary font-semibold text-2xl`}>
-						Hello, I'm <span css={tw`text-secondary`}>Prateek Karki</span>
+						Hello, I&apos;m
+						<span css={tw`text-secondary`}>Prateek Karki</span>
 					</p>
 					<p css={tw`text-primary text-5xl font-bold`}>Javascript Developer</p>
 					<p css={tw`text-light text-base font-normal mt-2`}>
@@ -30,11 +31,13 @@ function Intro() {
 					</p>
 					<div css={tw`flex gap-10 justify-center md:justify-start mt-4`}>
 						<button
+							type="button"
 							css={tw`w-40 rounded-full bg-primary shadow-primary font-semibold text-base py-4 text-white text-center`}
 						>
 							Hire Me
 						</button>
 						<button
+							type="button"
 							css={tw`w-40 rounded-full bg-secondary font-semibold text-base py-4 text-white shadow-secondary text-center `}
 						>
 							Get Resume

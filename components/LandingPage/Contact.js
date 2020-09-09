96d67c011a1381c0b7b14ba7f81407564408f8b5
Prateek Karki <pratiek.karki@gmail.com>
2020-09-09 23:53:26 +0545

    contact page wip

--- components/LandingPage/Contact.js
+++ components/LandingPage/Contact.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+import TitleBlock from './TitleBlock';
+
 function Contact() {
 	const [inputs, setInputs] = useState({
 		email: '',
@@ -50,37 +52,55 @@ function Contact() {
 
 	return (
 		<div>
-			<h1>Contact Me</h1>
-			<form onSubmit={handleOnSubmit}>
-				<label htmlFor="email">
-					Email:
-					<input
-						id="email"
-						type="email"
-						name="email"
-						required
-						onChange={handleOnChange}
-						value={inputs.email}
-					/>
-				</label>
-				<label htmlFor="message">
-					Message:
-					<textarea
-						id="message"
-						name="message"
-						onChange={handleOnChange}
-						value={inputs.message}
-					/>
-				</label>
-				<button type="submit" disabled={serverState.submitting}>
-					Submit
-				</button>
-				{serverState.status && (
-					<p className={!serverState.status.ok ? 'errorMsg' : ''}>
-						{serverState.status.msg}
-					</p>
-				)}
-			</form>
+			<TitleBlock title="Contact Me" subtitle="Send me a message" />
+			<div className="bg-main-light flex">
+				<div className="w-1/2">
+					{/* Address Mada Center 8th floor, 379 Hudson St, New York, NY 10018 US Lets Talk +1 */}
+					{/* 800 1236879 General Support contact@example.com */}
+				</div>
+				<div className="w-1/2 flex-col">
+					<form onSubmit={handleOnSubmit}>
+						<label className="w-full flex mt-4" htmlFor="email">
+							<p className="text-base text-primary text-right w-1/3">Email:</p>
+							<input
+								className="w-2/3 rounded-md mx-4"
+								id="email"
+								type="email"
+								name="email"
+								required
+								onChange={handleOnChange}
+								value={inputs.email}
+							/>
+						</label>
+						<label className="w-full flex mt-4" htmlFor="message">
+							<p className="text-base text-primary text-right w-1/3">Message:</p>
+							<textarea
+								className="w-2/3 rounded-md mx-4"
+								id="message"
+								name="message"
+								onChange={handleOnChange}
+								value={inputs.message}
+							/>
+						</label>
+
+						<div className="w-full flex mt-4">
+							<p className="text-base text-primary text-right w-1/3" />
+							<button
+								type="submit"
+								className="ml-1 bg-primary h-8 rounded-md text-white w-32"
+								disabled={serverState.submitting}
+							>
+								Submit
+							</button>
+						</div>
+						{serverState.status && (
+							<p className={!serverState.status.ok ? 'errorMsg' : ''}>
+								{serverState.status.msg}
+							</p>
+						)}
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 }

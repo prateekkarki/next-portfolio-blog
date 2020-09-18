577670738f5ee9592526de58bb9f81d022fee638
Prateek Karki <pratiek.karki@gmail.com>
2020-09-19 00:37:30 +0545

    using twin macro on more components

--- components/LandingPage/TitleBlock.js
+++ components/LandingPage/TitleBlock.js
@@ -1,37 +1,44 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
 import PropTypes from 'prop-types';
 
-import { css } from 'twin.macro';
+import tw from 'twin.macro';
 
 function TitleBlock({ title, subtitle }) {
 	return (
-		<div className="flex flex-col items-center text-center py-6">
-			<h2 className="font-bold text-primary text-5xl">{title}</h2>
+		<div css={tw`flex flex-col items-center text-center py-6`}>
+			<h2 css={tw`font-bold text-primary text-5xl`}>{title}</h2>
 			<p
-				className="font-normal text-base text-light"
-				css={css`
-					letter-spacing: 2.5px;
-					line-height: 19.2px;
-				`}
+				css={[
+					tw`font-normal text-base text-light`,
+					css`
+						letter-spacing: 2.5px;
+						line-height: 19.2px;
+					`,
+				]}
 			>
 				{subtitle}
 			</p>
-			<div className="w-64 relative h-6 mt-6">
+			<div css={tw`w-64 relative h-6 mt-6`}>
 				<div
-					className="w-full absolute top-0 bg-primary rounded-full h-1"
-					css={css`
-						top: 50%;
-						left: 50%;
-						transform: translate(-50%, -50%);
-					`}
+					css={[
+						tw`w-full absolute top-0 bg-primary rounded-full h-1`,
+						css`
+							top: 50%;
+							left: 50%;
+							transform: translate(-50%, -50%);
+						`,
+					]}
 				/>
 				<div
-					className="w-12 absolute top-0 bg-primary rounded-full h-3"
-					css={css`
-						top: 50%;
-						left: 50%;
-						transform: translate(-50%, -50%);
-					`}
+					css={[
+						tw`w-12 absolute top-0 bg-primary rounded-full h-3`,
+						css`
+							top: 50%;
+							left: 50%;
+							transform: translate(-50%, -50%);
+						`,
+					]}
 				/>
 			</div>
 		</div>

80625232c5a51a8a05bb6875de23588d16cfc1f1
Prateek Karki <pratiek.karki@gmail.com>
2020-09-19 00:31:15 +0545

    form updates

--- components/LandingPage/Contact/Contact.js
+++ components/LandingPage/Contact/Contact.js
@@ -11,29 +11,40 @@ function Contact() {
 			<TitleBlock title="Contact Me" subtitle="Send me a message" />
 			<div className="bg-main-light">
 				<div className="container mx-auto flex flex-col sm:flex-row">
-					<div className="w-full sm:w-1/2  py-6 text-center">
-						<div className="flex flex-col items-center">
-							<IoMdPin className="text-primary text-xl" />
-							<h3 className="font-bold text-primary">Address</h3>
-							<p className="text-light">New Baneshwor,</p>
-							<p className="text-light">Kathmandu, Nepal.</p>
-						</div>
-						<div className="flex flex-col items-center mt-6">
-							<IoIosCall className="text-primary text-xl" />
-							<h3 className="font-bold text-primary">Lets Talk</h3>
-							<a className="text-light hover:text-primary" href="tel:+977984241xxxx">
-								+977 984 241 XXXX
-							</a>
-						</div>
-						<div className="flex flex-col items-center mt-6">
-							<IoIosMail className="text-primary text-xl" />
-							<h3 className="font-bold text-primary">Write to me</h3>
-							<a
-								className="text-light hover:text-primary"
-								href="mailto:info@meetprateek.com"
-							>
-								info@meetprateek.com
-							</a>
+					<div className="w-full sm:w-1/2 py-6 text-center flex flex-col justify-center items-center">
+						<div className="">
+							<div className="flex flex-col items-center sm:flex-row sm:items-start">
+								<IoMdPin className="text-primary text-xl w-5 h-8" />
+								<div className="text-center sm:text-left sm:ml-4">
+									<h3 className="font-bold text-primary text-xl">Address</h3>
+									<p className="text-light">New Baneshwor,</p>
+									<p className="text-light">Kathmandu, Nepal.</p>
+								</div>
+							</div>
+							<div className="flex flex-col items-center sm:flex-row sm:items-start mt-6">
+								<IoIosCall className="text-primary text-xl w-5 h-8" />
+								<div className="text-center sm:text-left sm:ml-4">
+									<h3 className="font-bold text-primary text-xl">Lets Talk</h3>
+									<a
+										className="text-light hover:text-primary"
+										href="tel:+977984241xxxx"
+									>
+										+977 984 241 XXXX
+									</a>
+								</div>
+							</div>
+							<div className="flex flex-col items-center sm:flex-row sm:items-start mt-6">
+								<IoIosMail className="text-primary text-xl w-5 h-8" />
+								<div className="text-center sm:text-left sm:ml-4">
+									<h3 className="font-bold text-primary text-xl">Write to me</h3>
+									<a
+										className="text-light hover:text-primary"
+										href="mailto:info@meetprateek.com"
+									>
+										info@meetprateek.com
+									</a>
+								</div>
+							</div>
 						</div>
 					</div>
 					<div className="w-full sm:w-1/2 flex flex-col items-center py-6">

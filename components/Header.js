b28b47d8eb97aae345e0c62c363a64748f0ac3f9
Prateek Karki <pratiek.karki@gmail.com>
2020-09-19 17:45:38 +0545

    header scroll bugfix

--- components/Header.js
+++ components/Header.js
@@ -1,57 +1,61 @@
 /** @jsx jsx * */
 import { jsx } from '@emotion/core';
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import anime from 'animejs';
 import tw from 'twin.macro';
+import { throttle } from 'lodash';
 
 import Nav from './Nav';
 
 function Header({ categories }) {
-	const [lastScrollTop, setLastScrollTop] = useState(0);
-	const [isUp, setIsUp] = useState(false);
+	const lastScrollTop = useRef(0);
+	const isUp = useRef(false);
 	const headerRef = useRef(null);
 
 	useEffect(() => {
 		const scrollCheck = () => {
 			const st = window.pageYOffset || document.documentElement.scrollTop;
-			if (st > lastScrollTop) {
-				if (!isUp && st > headerRef.current.clientHeight) {
+			// console.log(st, isUp.current);
+			if (st > lastScrollTop.current) {
+				if (!isUp.current && st > headerRef.current.clientHeight) {
 					// console.log('downscroll');
 					anime({
 						targets: [headerRef.current.parentElement.querySelectorAll('header')],
 						translateY: [0, '-100%'],
 						easing: 'easeInOutQuad',
 						duration: 400,
-						complete: () => {
-							setIsUp(true);
+						begin: () => {
+							isUp.current = true;
 						},
 					});
 				}
-			} else if (isUp) {
+			} else if (isUp.current) {
 				// console.log('upscroll');
 				anime({
 					targets: [headerRef.current],
 					translateY: ['-100', '0%'],
 					easing: 'easeInOutQuad',
 					duration: 400,
-					complete: () => {
-						setIsUp(false);
+					begin: () => {
+						isUp.current = false;
 					},
 				});
 			}
-			setLastScrollTop(st <= 0 ? 0 : st);
+			lastScrollTop.current = st <= 0 ? 0 : st;
 		};
 
-		window.addEventListener('scroll', scrollCheck);
+		const throttled = throttle(scrollCheck, 10, { leading: true, trailing: false });
+
+		window.addEventListener('scroll', throttled);
 
 		return () => {
-			window.removeEventListener('scroll', scrollCheck);
+			window.removeEventListener('scroll', throttled);
 		};
 	});
 
 	return (
-		<header css={tw`bg-main-light z-10 sticky top-0`} ref={headerRef}>
+		<header css={tw`bg-main-light z-10 sticky top-0 shadow-2xl`} ref={headerRef}>
 			<Nav categories={categories} />
 		</header>
 	);

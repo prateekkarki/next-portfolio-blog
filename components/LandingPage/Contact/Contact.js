da7a5b46ae1fdf216f8ee248012bddacf557984f
Prateek Karki <pratiek.karki@gmail.com>
2020-09-20 01:04:31 +0545

    changing old contact page concept

--- components/LandingPage/Contact/Contact.js
+++ components/LandingPage/Contact/Contact.js
@@ -1,6 +1,5 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { IoMdPin, IoIosCall, IoIosMail } from 'react-icons/io';
 
 import TitleBlock from '../TitleBlock';
 import ContactForm from './ContactForm';
@@ -11,40 +10,16 @@ function Contact() {
 			<TitleBlock title="Contact Me" subtitle="Send me a message" />
 			<div className="bg-main-light">
 				<div className="container mx-auto flex flex-col sm:flex-row">
-					<div className="w-full sm:w-1/2 py-6 text-center flex flex-col justify-center items-center">
+					<div className="w-full sm:w-1/2 py-6 px-3 flex flex-col justify-center items-center text-center sm:justify-start md:items-start sm:text-left">
 						<div className="">
-							<div className="flex flex-col items-center sm:flex-row sm:items-start">
-								<IoMdPin className="text-primary text-xl w-5 h-8" />
-								<div className="text-center sm:text-left sm:ml-4">
-									<h3 className="font-bold text-primary text-xl">Address</h3>
-									<p className="text-light">New Baneshwor,</p>
-									<p className="text-light">Kathmandu, Nepal.</p>
-								</div>
-							</div>
-							<div className="flex flex-col items-center sm:flex-row sm:items-start mt-6">
-								<IoIosCall className="text-primary text-xl w-5 h-8" />
-								<div className="text-center sm:text-left sm:ml-4">
-									<h3 className="font-bold text-primary text-xl">Lets Talk</h3>
-									<a
-										className="text-light hover:text-primary"
-										href="tel:+977984241xxxx"
-									>
-										+977 984 241 XXXX
-									</a>
-								</div>
-							</div>
-							<div className="flex flex-col items-center sm:flex-row sm:items-start mt-6">
-								<IoIosMail className="text-primary text-xl w-5 h-8" />
-								<div className="text-center sm:text-left sm:ml-4">
-									<h3 className="font-bold text-primary text-xl">Write to me</h3>
-									<a
-										className="text-light hover:text-primary"
-										href="mailto:info@meetprateek.com"
-									>
-										info@meetprateek.com
-									</a>
-								</div>
-							</div>
+							<h1 className="font-bold text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+								Get in touch
+							</h1>
+							<p className="text-white">
+								I love to develop things for the web. I also speak, teach, and
+								consult at tech companies and startups. If you want to get started
+								with a project or just need my help, feel free to contact me.
+							</p>
 						</div>
 					</div>
 					<div className="w-full sm:w-1/2 flex flex-col items-center py-6">

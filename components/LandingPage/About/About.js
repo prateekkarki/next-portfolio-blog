bb819e91643bf946df08cf2f21c46eb9369d32a1
Prateek Karki <pratiek.karki@gmail.com>
2020-09-22 23:25:44 +0545

    about section styling

--- components/LandingPage/About/About.js
+++ components/LandingPage/About/About.js
@@ -9,18 +9,27 @@ function About() {
 		<div>
 			{/* <TitleBlock title="About Me" subtitle="Who am I?" /> */}
 			<div css={tw`block md:flex`}>
-				<div css={tw`hidden md:block md:w-1/2`}>
+				<div css={tw`hidden md:flex items-center md:w-1/2`}>
 					<DevOnSofa />
 				</div>
 				<div css={tw`text-center text-light py-4 md:w-1/2 md:text-left`}>
-					<h4 css={tw`px-3 md:px-6 text-2xl`}>
-						<strong>Hello stranger,</strong>
-					</h4>
-					<p css={tw`text-base font-normal mt-4 px-3 md:px-6`}>
-						I&apos;m Prateek, a web developer from Kathmandu, Nepal.
+					<h1
+						css={tw`px-3 md:px-6 font-bold text-white whitespace-no-wrap text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-huge`}
+					>
+						About Me
+					</h1>
+					<p css={tw`px-3 md:px-6 text-white text-sm md:text-base lg:text-lg`}>
+						I&apos;m Prateek Karki, a web developer from Kathmandu, Nepal.
 						<br />I enjoy creating things that live on the internet, whether that be
-						websites, applications, or anything in between. My goal is to always build
-						products that provide pixel-perfect, performant experiences.
+						websites, applications, or anything in between. My goal is to build products
+						that provide pixel-perfect, performant experiences.
+					</p>
+					<p css={tw`mt-4 px-3 md:px-6 text-white text-sm md:text-base lg:text-lg`}>
+						With my experiences as Solution architect, Senior software engineer and
+						Project lead with highly reputed companies, I can think in terms of systems
+						architecture, feasibility and risks for any given project. I believe in long
+						term relationships with the every client and my primarily focus on long term
+						projects.
 					</p>
 				</div>
 			</div>

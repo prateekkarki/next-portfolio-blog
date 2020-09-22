783782cfbb3b1235c431a051f5261463ab892ec9
Prateek Karki <pratiek.karki@gmail.com>
2020-09-22 22:48:03 +0545

    developer svg component

---
+++ components/LandingPage/About/devOnSofa.css.js
@@ -0,0 +1,12 @@
+import { css } from '@emotion/core';
+
+const ProgrammerCss = css`
+	* {
+		transform-box: fill-box;
+	}
+	#leaves {
+		transform-origin: bottom;
+	}
+`;
+
+export default ProgrammerCss;

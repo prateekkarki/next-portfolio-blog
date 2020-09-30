da8a6f6b914b69610c48f30b345435dc260fe021
Prateek Karki <pratiek.karki@gmail.com>
2020-09-30 16:35:27 +0545

    lint fix

--- components/LandingPage/Skewed.js
+++ components/LandingPage/Skewed.js
@@ -1,24 +1,28 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import tw from "twin.macro"
+import tw from 'twin.macro';
 
-function Skewed({ children }){
-    return (
+function Skewed({ children }) {
+	return (
 		<React.Fragment>
-            <div
-                css={[
-                    tw`absolute w-full h-full bg-main-light`,
-                    css`
-                        transform: skewY(-5deg);
-                    `,
-                ]}
-            />
-            <div css={tw` relative container mx-auto py-16`}>
-                {children}
-            </div>
-        </React.Fragment>
-    );
+			<div
+				css={[
+					tw`absolute w-full h-full bg-main-light`,
+					css`
+						transform: skewY(-5deg);
+					`,
+				]}
+			/>
+			<div css={tw` relative container mx-auto py-16`}>{children}</div>
+		</React.Fragment>
+	);
 }
 
-export default Skewed
+Skewed.propTypes = {
+	children: PropTypes.element.isRequired,
+};
+
+export default Skewed;

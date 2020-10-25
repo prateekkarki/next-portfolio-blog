63a449f5c6be7b2806b9286fe847cc789ee2196a
Prateek Karki <pratiek.karki@gmail.com>
2020-10-25 22:33:51 +0545

    update on card component

--- components/BlogPosts/Card.js
+++ components/BlogPosts/Card.js
@@ -7,63 +7,68 @@ import tw from 'twin.macro';
 
 import CategoryToIcon from './CategoryToIcon';
 
+const Tag = tw.span`
+  inline-block bg-gray-200 rounded-full 
+  px-3 py-1 mr-2 mb-2
+  text-sm font-semibold text-main-dark `;
 const Card = ({ article }) => (
-  <Link href="/article/[aid]" as={`/article/${article.slug}`} passHref>
-    <div
-      css={tw`w-full overflow-hidden shadow-md hover:shadow-lg cursor-pointer`}
-    >
-      {article.thumbnail && article.thumbnail.url.slice(0, 1) !== '/' && (
-        <CloudinaryContext
-          cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_NAME}
-          css={tw`rounded-t-md w-full h-64 overflow-hidden`}
-        >
-          <Image
-            secure="true"
-            responsive
-            width="auto"
-            crop="scale"
-            css={tw`
+  <div css={tw`w-full overflow-hidden shadow-md hover:shadow-lg`}>
+    <Link href="/article/[aid]" as={`/article/${article.slug}`} passHref>
+      <>
+        {article.thumbnail && article.thumbnail.url.slice(0, 1) !== '/' && (
+          <CloudinaryContext
+            cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_NAME}
+            css={tw`rounded-t-md w-full h-64 overflow-hidden cursor-pointer`}
+          >
+            <Image
+              secure="true"
+              responsive
+              width="auto"
+              crop="scale"
+              css={tw`
               w-full h-64 object-cover object-center 
               transition-all duration-500 ease-out transform hover:scale-125 
               rounded-t-md origin-center
             `}
-            responsiveUseBreakpoints="true"
-            publicId={article.thumbnail.url.split('/').pop()}
+              responsiveUseBreakpoints="true"
+              publicId={article.thumbnail.url.split('/').pop()}
+            >
+              <Placeholder type="pixelate" />
+            </Image>
+          </CloudinaryContext>
+        )}
+        {article.thumbnail && article.thumbnail.url.slice(0, 1) === '/' && (
+          <div
+            css={tw`rounded-t-md w-full h-64 overflow-hidden cursor-pointer`}
           >
-            <Placeholder type="pixelate" />
-          </Image>
-        </CloudinaryContext>
-      )}
-      {article.thumbnail && article.thumbnail.url.slice(0, 1) === '/' && (
-        <div css={tw`rounded-t-md w-full h-64 overflow-hidden`}>
-          <img
-            css={tw`
+            <img
+              css={tw`
               w-full h-64 object-cover object-center 
               transition-all duration-500 ease-out transform hover:scale-125
               rounded-t-md origin-center
             `}
-            alt={`cover for article: ${article.title}`}
-            src={`${process.env.API_URL}${article.thumbnail.url}`}
-          />
-        </div>
-      )}
-      {!article.thumbnail && <CategoryToIcon category={article.category} />}
-      <div css={tw`px-6 py-4 bg-main-light`}>
-        <h2 css={tw`font-bold text-light text-xl mb-2`}>{article.title}</h2>
-      </div>
-      <div css={tw`px-6 pb-2 bg-main-light rounded-b-md`}>
-        <span
-          css={tw`
-            inline-block bg-gray-200 rounded-full 
-            px-3 py-1 mr-2 mb-2
-            text-sm font-semibold text-main-dark 
-          `}
-        >
-          #{article.category ? article.category.name : ''}
-        </span>
-      </div>
+              alt={`cover for article: ${article.title}`}
+              src={`${process.env.API_URL}${article.thumbnail.url}`}
+            />
+          </div>
+        )}
+      </>
+    </Link>
+    {!article.thumbnail && <CategoryToIcon category={article.category} />}
+    <div css={tw`px-6 pt-4 bg-main-light`}>
+      <Link href="/article/[aid]" as={`/article/${article.slug}`} passHref>
+        <a css={tw`font-bold text-light text-xl mb-0`}>{article.title}</a>
+      </Link>
     </div>
-  </Link>
+    <div css={tw`px-6 pb-2 bg-main-light rounded-b-md`}>
+      <p css={tw`font-semibold text-light`}>{article.category?.name || ''}</p>
+    </div>
+    <div css={tw`px-6 pb-2 bg-main-light rounded-b-md`}>
+      {article.tags.map((tag) => (
+        <Tag>#{tag.name}</Tag>
+      ))}
+    </div>
+  </div>
 );
 
 Card.propTypes = {

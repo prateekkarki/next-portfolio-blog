537f1fdbb89e5ac0652cafd634d861c40e167ef7
Prateek Karki <pratiek.karki@gmail.com>
2021-04-24 22:17:21 +0545

    some refactors

--- components/Layout/Main/index.js
+++ components/Layout/Main/index.js
@@ -1,5 +1,4 @@
 import { GlobalStyles } from 'twin.macro';
-import anime from 'animejs';
 import { Slide } from 'react-toastify';
 
 import { Transition, TransitionGroup } from 'react-transition-group';
@@ -8,39 +7,10 @@ import Header from './Header';
 import Footer from './Footer';
 
 import { StyledToastContainer, InnerContainer, MainContainer } from './styles';
+import transitionConfig from './styles/transitionConfig';
 
 import { useLocalStorage } from '../../../hooks';
 import { FullpageLoader, MetaHead } from '../..';
-
-const transitionConfig = {
-  timeout: 650,
-  unmountOnExit: true,
-  onEnter: (node) => {
-    const el = node;
-    el.style.display = 'none';
-  },
-  onEntered: (node) => {
-    const el = node;
-    el.style.display = 'block';
-    anime({
-      targets: node.querySelectorAll('.fullpage-loader>div'),
-      translateX: ['0%', '100%'],
-      duration: 500,
-      easing: 'easeInOutSine',
-      delay: anime.stagger(150),
-    });
-  },
-  onExit: (node) => {
-    anime({
-      targets: node.querySelectorAll('.fullpage-loader>div'),
-      scaleX: [0, 1],
-      translateX: ['0%', '0%'],
-      duration: 500,
-      easing: 'easeInOutSine',
-      delay: anime.stagger(150),
-    });
-  },
-};
 
 function Main({ children, pathname }) {
   const [isDark, setIsDark] = useLocalStorage('isDark', true);

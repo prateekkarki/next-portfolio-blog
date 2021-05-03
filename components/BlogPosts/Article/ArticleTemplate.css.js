f8adc57dcd1dd9eb060a8cd9facb4044d4061c7f
prateek karki <pratiek.karki@gmail.com>
2021-05-03 17:37:27 +0545

    updating dependancies

--- components/BlogPosts/Article/ArticleTemplate.css.js
+++ components/BlogPosts/Article/ArticleTemplate.css.js
@@ -2,29 +2,6 @@ import { theme, css } from 'twin.macro';
 
 export default [
   css`
-    .dark &{
-      .article-main {
-        h1 {
-          border-bottom: 0.25rem solid ${theme`colors.mainDark.100`};
-        }
-        a {
-          color: ${theme`colors.primary`};
-          &:hover {
-            color: ${theme`colors.mainDark.100`};
-          }
-        }
-        pre {
-          background-color: ${theme`colors.mainDark.100`};
-        }
-        code {
-          color: ${theme`colors.mainDark.800`};
-          background-color: ${theme`colors.mainDark.100`};
-          &.language-js {
-            background-color: ${theme`colors.mainDark.100`};
-          }
-        }
-      }
-    }
     .article-main {
       padding-top: 1.5rem;
       p,
@@ -84,19 +61,22 @@ export default [
       }
       pre {
         border-radius: 0.375rem;
-        padding: 1.25rem 0.75rem;
-        background-color: ${theme`colors.mainDark.100`};
-        overflow: auto hidden;
-        margin-bottom: 1.5rem;
       }
       code {
-        color: ${theme`colors.mainDark.800`};
-        background-color: ${theme`colors.mainDark.100`};
         border-radius: 0.375rem;
-        &.language-js {
-          padding: 1.25rem 0.75rem;
-          background-color: ${theme`colors.mainDark.100`};
-          display: block;
+      }
+    }
+    
+    .dark &{
+      .article-main {
+        h1 {
+          border-bottom: 0.25rem solid ${theme`colors.mainDark.100`};
+        }
+        a {
+          color: ${theme`colors.primary`};
+          &:hover {
+            color: ${theme`colors.mainDark.100`};
+          }
         }
       }
     }

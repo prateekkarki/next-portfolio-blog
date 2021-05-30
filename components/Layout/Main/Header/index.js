03672f31d3736034651cd84a52414ebdc34a0fd8
Prateek Karki <pratiek.karki@gmail.com>
2021-05-30 21:30:35 +0545

    hero animation update

--- components/Layout/Main/Header/index.js
+++ components/Layout/Main/Header/index.js
@@ -7,7 +7,7 @@ function Header() {
     <header
       css={tw`
         bg-mainLight-200 dark:bg-mainDark-200
-        w-full z-10 fixed top-0 shadow-2xl dark:shadow-lg
+        w-full z-10 fixed top-0 shadow-lg 
       `}
     >
       <Nav />
--- components/pages/LandingPage/Intro/styles.js
+++ components/pages/LandingPage/Intro/styles.js
@@ -16,9 +16,25 @@ export const IntroContainer = styled.div(css`
 export const ButtonsHolder = styled.div([
   tw`flex gap-10 justify-center md:justify-start mt-16`,
   css`
+    transform: scale(0);
     width: 100%;
     @media (min-width: 768px) {
       width:min-content;
+    }
+  `,
+]);
+
+export const LoaderBar = styled.div(tw`inline-block absolute w-full h-full`);
+
+export const AnimatedText = styled.div([
+  tw`inline-block relative overflow-hidden`,
+  css`
+    ${LoaderBar}{
+      transform: translate(-100%);
+    }
+    p{
+      opacity: 0;
+      display: inline-block;
     }
   `,
 ]);

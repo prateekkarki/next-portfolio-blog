e891e91a4de6014a8700f748f8954a6c34f2d55e
Prateek Karki <pratiek.karki@gmail.com>
2023-03-16 18:30:01 +0545

    add external link

--- components/BlogPosts/Card/MiniCard/index.js
+++ components/BlogPosts/Card/MiniCard/index.js
@@ -11,8 +11,20 @@ import { ImageWindow, Tag, CardContainer } from '../styles';
 
 import TagToIcon from '../TagToIcon';
 
-const Card = ({ article, dark }) => {
-  const url = `${article?.thumbnail?.url.replace(/^.*[\\/]/, '')}`;
+export const LinkWrapper = ({ children, article }) => {
+  if (article.isExternal) {
+    return (
+      <a
+        href={article.externalUrl}
+        target="_blank"
+        title={article.title}
+        tw="max-w-full w-full"
+        rel="noreferrer"
+      >
+        {children}
+      </a>
+    );
+  }
 
   return (
     <Link href="/article/[aid]" passHref as={`/article/${article.slug}`}>
@@ -20,56 +32,67 @@ const Card = ({ article, dark }) => {
         href={`/article/${article.slug}`}
         title={article.title}
         tw="max-w-full w-full"
+        rel="noreferrer"
       >
-        <CardContainer dark={dark} mini tw="flex flex-row">
-          <div tw="width[35%] ">
-            <Fragment>
-              {article.thumbnail && (
-                <div css={tw`w-full h-full flex justify-center items-center `}>
-                  <ImageWindow>
-                    <Image
-                      src={url}
-                      loader={cloudinaryLoader}
-                      placeholder="blur"
-                      blurDataURL={cloudinaryLoader({
-                        src: url,
-                        width: 265,
-                        blur: true,
-                      })}
-                      width={265}
-                      height={150}
-                      layout="responsive"
-                      alt={article.title}
-                      title={article.title}
-                    />
-                  </ImageWindow>
-                </div>
-              )}
-            </Fragment>
-            {!article.thumbnail && <TagToIcon tags={article.tags} />}
-          </div>
-          <div css={[tw`width[64%] flex flex-col justify-center`]}>
-            <div css={tw`px-6 pt-4`}>
-              <p
-                css={tw`font-bold text-light-700 dark:text-dark-700 text-xl mb-0`}
-              >
-                {article.title}
-              </p>
-            </div>
-            <div css={tw`px-6 pb-2`}>
-              <p css={tw`text-light-600 dark:text-dark-600`}>
-                {article.category?.title || ''}
-              </p>
-            </div>
-            <div css={tw`px-6 pb-2 mt-4`}>
-              {article.tags.map((tag) => (
-                <Tag key={`tag-${tag.slug}`}>#{tag.title}</Tag>
-              ))}
-            </div>
-          </div>
-        </CardContainer>
+        {children}
       </a>
     </Link>
+  );
+};
+
+const Card = ({ article, dark }) => {
+  const url = `${article?.thumbnail?.url.replace(/^.*[\\/]/, '')}`;
+
+  return (
+    <LinkWrapper article={article}>
+      <CardContainer dark={dark} mini tw="flex flex-row">
+        <div tw="width[35%] ">
+          <Fragment>
+            {article.thumbnail && (
+              <div css={tw`w-full h-full flex justify-center items-center `}>
+                <ImageWindow>
+                  <Image
+                    src={url}
+                    loader={cloudinaryLoader}
+                    placeholder="blur"
+                    blurDataURL={cloudinaryLoader({
+                      src: url,
+                      width: 265,
+                      blur: true,
+                    })}
+                    width={265}
+                    height={150}
+                    layout="responsive"
+                    alt={article.title}
+                    title={article.title}
+                  />
+                </ImageWindow>
+              </div>
+            )}
+          </Fragment>
+          {!article.thumbnail && <TagToIcon tags={article.tags} />}
+        </div>
+        <div css={[tw`width[64%] flex flex-col justify-center`]}>
+          <div css={tw`px-6 pt-4`}>
+            <p
+              css={tw`font-bold text-light-700 dark:text-dark-700 text-xl mb-0`}
+            >
+              {article.title} {article.isExternal ? '⧉' : ''}
+            </p>
+          </div>
+          <div css={tw`px-6 pb-2`}>
+            <p css={tw`text-light-600 dark:text-dark-600`}>
+              {article.category?.title || ''}
+            </p>
+          </div>
+          <div css={tw`px-6 pb-2 mt-4`}>
+            {article.tags.map((tag) => (
+              <Tag key={`tag-${tag.slug}`}>#{tag.title}</Tag>
+            ))}
+          </div>
+        </div>
+      </CardContainer>
+    </LinkWrapper>
   );
 };
 

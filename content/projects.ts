b9e8ab0fa85a2991fdfde8179deaf70034b2a668
Prateek Karki <pratiek.karki@gmail.com>
2025-08-14 23:43:50 +0545

    add projects

--- content/projects.ts
+++ content/projects.ts
@@ -18,10 +18,6 @@ export const projects: Project[] = [
     image: 'v1755090854/Technologies/icons/nextjs/nextjs_z80rkh.svg',
     link: 'https://canadiantrainvacations.com',
     github: 'https://github.com/prateekkarki/next-portfolio-blog',
-    gallery: [
-      '/images/projects/portfolio.jpg',
-      '/images/projects/portfolio.jpg',
-    ],
     backgroundColor: '#ffffff',
     isFeatured: true,
   },
@@ -32,10 +28,6 @@ export const projects: Project[] = [
       'A cross-platform progressive web app (PWA) to visualize and interpret large-scale industrial data. The application, accessible on both web and mobile, transforms complex raw datasets into clear, actionable insights. By streamlining data analysis and emphasizing operational performance, the app enables field teams and decision-makers to identify key trends and take informed action in real-time. This project significantly improved data accessibility, responsiveness, and overall efficiency for drilling operations.',
     technologies: ['React', 'Node.js', 'MongoDB', 'Stripe'],
     image: 'v1755091108/Technologies/icons/logo_bdd5ah.svg',
-    // gallery: [
-    //   '/images/projects/ecommerce.jpg',
-    //   '/images/projects/ecommerce.jpg',
-    // ],
     isFeatured: true,
     backgroundColor: '#ffffff',
   },
@@ -70,6 +62,22 @@ export const projects: Project[] = [
   },
   {
     id: 5,
+    title: 'timeTracko: A workforce analytics and productivity software',
+    description:
+      "timeTracko is a workforce analytics and productivity software that helps you track your team's productivity and efficiency. It's a comprehensive software that helps you track your team's productivity and efficiency.",
+    technologies: ['React', 'Redux', 'Redux Saga'],
+    image: 'v1755090862/Technologies/icons/react/react-original_anyvwp.svg',
+    gallery: [
+      'v1755192782/Works/timeTracko/screencast-feature_nh6jkc.png',
+      'v1755192781/Works/timeTracko/attendance-feature_akhj1i.png',
+      'v1755192781/Works/timeTracko/app-feature_biaqeu.png',
+      'v1755192780/Works/timeTracko/time-feature_ikmb6a.png',
+      'v1755192780/Works/timeTracko/automatic-timetracking-feature_utmfrr.png',
+    ],
+    backgroundColor: '#ffffff',
+  },
+  {
+    id: 7,
     title: 'My Personal Website',
     description:
       "This is my website and blog. I post about new things I've recently learned and about my experiences as a remote developer.  In my blog post, you can read about memoization in JavaScript, using AI to generate blog ideas, avoiding glitchy CSS transitions on Hover, and getting started with Strapi.",
@@ -80,7 +88,7 @@ export const projects: Project[] = [
     backgroundColor: '#ffffff',
   },
   {
-    id: 6,
+    id: 8,
     title: 'Magento 2 CMS Banner Extension',
     description:
       'This is a Magento 2 extension that allows you to create and manage banners on your website. Convenient and easy to use extension if you need to add banners to your cms pages like about us or homepage in your Magento store.',
@@ -90,13 +98,30 @@ export const projects: Project[] = [
     backgroundColor: '#ffffff',
   },
   {
-    id: 7,
+    id: 9,
     title: 'Strapi blog backend',
     description:
       'This is a Strapi blog backend that allows you to create and manage blog posts on your website. I built this backend for my personal website to have a fully functional blog backend. I used Strapi headless CMS to build this backend.',
     technologies: ['Strapi', 'TypeScript', 'PostgreSQL', 'GraphQL'],
     image: 'v1755096828/Technologies/icons/Strapi.full.logo.dark_zxhyz3.svg',
     github: 'https://github.com/prateekkarki/next-portfolio-blog',
+    backgroundColor: '#ffffff',
+  },
+  {
+    id: 10,
+    title: 'Early grade learning web app',
+    description:
+      'This is a web app built on React using JavaScript. The clients provided me with the required designs and illustrations on Adobe Photoshop and Illustrator, and I translated the designs into a fully functional responsive web app.',
+    technologies: ['React', 'JavaScript', 'GSAP', 'React-Router', 'React DnD'],
+    image: 'v1755090862/Technologies/icons/react/react-original_anyvwp.svg',
+    link: 'https://egr.olenepal.org/',
+    gallery: [
+      'v1755193377/Works/egr/egr-home_auxmsv.png',
+      'v1755193375/Works/egr/egr-stories_pbxhte.png',
+      'v1755193374/Works/egr/egr-games_drinmh.png',
+      'v1755193378/Works/egr/egr-baloon_n0kins.png',
+      'v1755193376/Works/egr/egr-balloon-burst_kdmnzz.png',
+    ],
     backgroundColor: '#ffffff',
   },
 ];
